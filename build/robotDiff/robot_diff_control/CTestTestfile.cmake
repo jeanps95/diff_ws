4b1b6256246c15038f0b3e0817b912b856c87472
Jean Paulo Silva <jeanps95@gmail.com>
2019-12-19 16:40:42 -0300

    First commit done right

--- build/robotDiff/robot_diff_control/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jean/diff_ws/src/robotDiff/robot_diff_control
-# Build directory: /home/jean/diff_ws/build/robotDiff/robot_diff_control
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
